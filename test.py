f8adcdbfb37d736d88d532a22c66fbb041cc3ec9
qqaazz0222 <qqaazz0222@naver.com>
2024-07-23 23:05:52 +0900

    Remove: Test files

--- test.py
+++
@@ -1,8 +0,0 @@
-# from db.controller_mysql import MysqlDB
-# from services import location
-
-# db = MysqlDB()
-# db.connect()
-
-# # location.create(db, 'test')
-# log.update_cctv(db, 10, 10)
--- test_send.py
+++
@@ -1,9 +0,0 @@
-import time
-from db.controller import MysqlDB, RedisDB, RedisMQ
-from utils.time import now, timestamp
-
-mq = RedisMQ()
-
-now = now()
-print({'event': '자해', 'location': '유치장1', 'occurred_at': timestamp(now)})
-# mq.send('event', {'event': '자해', 'location': '유치장1', 'occurred_at': now})
